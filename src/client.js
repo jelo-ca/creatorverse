c47732b6e00a332a14c70dea7627b2b10d07cf96
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-02-03 11:24:10 -0800

    Initial Commit

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import { createClient } from "some-library";
+
+const URL = "https://hkiclqyiywmbcinnpzea.supabase.co";
+const API_KEY = "sb_publishable_kPu6Rj9dexY2SHQW_kSaeA_cRU8UeO4";
+const supabase = createClient(URL, API_KEY);
+
+export default supabase;
