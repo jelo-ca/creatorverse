9a14a7a4a9a0250785432b7a93dfd2574c412c46
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-02-03 11:45:17 -0800

    dependency fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
+import { useRoutes } from "react-router-dom";
 import "./App.css";
+import AddCreators from "./pages/AddCreators";
+import EditCreators from "./pages/EditCreators";
+import ShowCreators from "./pages/ShowCreators";
+import ViewCreators from "./pages/ViewCreators";
+
+const routes = [
+  { path: "/", element: <ShowCreators /> },
+  { path: "/add", element: <AddCreators /> },
+  { path: "/edit", element: <EditCreators /> },
+  { path: "/show", element: <ShowCreators /> },
+  { path: "/view", element: <ViewCreators /> },
+];
 
 function App() {
-  return "Hello World";
+  const routeElements = useRoutes(routes);
+  return routeElements;
 }
 
 export default App;
